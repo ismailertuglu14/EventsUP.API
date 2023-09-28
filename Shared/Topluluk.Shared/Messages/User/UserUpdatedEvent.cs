9b3422230db913f0143b82c8fc5e35d27ea6f20e
İsmail Ertuğlu <ismailertgl@gmail.com>
2023-09-28 22:12:16 +0300

    Event created

--- Shared/Topluluk.Shared/Messages/User/UserUpdatedEvent.cs
+++ Shared/Topluluk.Shared/Messages/User/UserUpdatedEvent.cs
@@ -5,15 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using Topluluk.Shared.Enums;
 
-namespace Topluluk.Shared.Dtos
+namespace Topluluk.Shared.Messages.User
 {
-    public record User
+    public class UserUpdatedEvent
     {
         public string Id { get; set; }
         public string FullName { get; set; }
         public string? UserName { get; set; }
         public string? ProfileImage { get; set; }
         public GenderEnum Gender { get; set; }
-
     }
 }
