efd3cf82ca7543ccfff43f10b2b4cbbedaf4db7a
İsmail Ertuğlu <ismailertgl@gmail.com>
2023-09-29 14:06:12 +0300

    Base Service implementations

--- Services/Event/Topluluk.Services.EventAPI.Services/Implementation/EventCommentService.cs
+++ Services/Event/Topluluk.Services.EventAPI.Services/Implementation/EventCommentService.cs
@@ -3,39 +3,32 @@ using Microsoft.AspNetCore.Http;
 using RestSharp;
 using Topluluk.Services.EventAPI.Data.Interface;
 using Topluluk.Services.EventAPI.Model.Dto;
-using Topluluk.Services.EventAPI.Model.Dto.Http;
 using Topluluk.Services.EventAPI.Model.Entity;
 using Topluluk.Services.EventAPI.Services.Interface;
-using Topluluk.Shared.Constants;
+using Topluluk.Shared.BaseModels;
 using Topluluk.Shared.Dtos;
-using Topluluk.Shared.Helper;
 using ResponseStatus = Topluluk.Shared.Enums.ResponseStatus;
 
 namespace Topluluk.Services.EventAPI.Services.Implementation;
 
-public class EventCommentService : IEventCommentService
+public class EventCommentService : BaseService, IEventCommentService
 {
     
     private readonly IMapper _mapper;
     private readonly RestClient _client;
     private readonly IEventCommentRepository _commentRepository;
-    private readonly IHttpContextAccessor _httpContextAccessor;
-    public EventCommentService(IMapper mapper, IEventCommentRepository commentRepository, IHttpContextAccessor httpContextAccessor)
+    public EventCommentService(IMapper mapper, IEventCommentRepository commentRepository, IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
     {
         _mapper = mapper;
         _client = new RestClient();
         _commentRepository = commentRepository;
-        _httpContextAccessor = httpContextAccessor;
     }
-
-    private string Token => _httpContextAccessor.HttpContext.Request.Headers["Authorization"];
-
 
     public async Task<Response<string>> CreateComment(string userId, CommentCreateDto dto)
     {
         try
         {
-            User? user = await HttpRequestHelper.GetUser(Token);
+            User? user = await GetCurrentUserAsync();
             if (user == null) throw new UnauthorizedAccessException();
 
             EventComment comment = _mapper.Map<EventComment>(dto);
--- Shared/Topluluk.Shared/BaseModels/IBaseService.cs
+++ Shared/Topluluk.Shared/BaseModels/IBaseService.cs
@@ -6,7 +6,16 @@ namespace Topluluk.Shared.BaseModels
     public interface IBaseService
     {
         IHttpContextAccessor _httpContextAccessor { get; set; }
+        
+        /// <summary>
+        /// This token is taken from header of request.
+        /// </summary>
         string Token { get; }
+
+        /// <summary>
+        /// This function returns current user with Token in header of request.
+        /// </summary>
+        /// <returns></returns>
         Task<User?> GetCurrentUserAsync();
     }
 }
