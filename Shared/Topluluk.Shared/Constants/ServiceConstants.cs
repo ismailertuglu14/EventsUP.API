04421d9bfb3ed586da8d7985db35680f5ec8d756
İsmail Ertuğlu <ismailertgl@gmail.com>
2024-01-07 14:20:34 +0300

    Api Gateway url refactor by environment sensitive

--- Shared/Topluluk.Shared/Constants/ServiceConstants.cs
+++ Shared/Topluluk.Shared/Constants/ServiceConstants.cs
@@ -3,7 +3,22 @@ namespace Topluluk.Shared.Constants
 {
 	public static class ServiceConstants
 	{
-		public static string API_GATEWAY = "https://localhost:7149/api";
+		public static string API_GATEWAY
+		{
+			get
+			{
+				string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? 
+					throw new ArgumentNullException("ASPNETCORE_ENVIRONMENT", "ASPNETCORE_ENVIRONMENT variable is can not be null");
+                return environment switch
+                {
+                    "Development" => "https://localhost:7149/api",
+                    "PreProduction" => "https://toplulukapi.azurewebsites.net/api",
+                    "Production" => "https://toplulukapi.azurewebsites.net/api",
+                    _ => "https://toplulukapi.azurewebsites.net/api"
+                };
+            }
+		}
+			
 	}
 }
 
